4660ec1f8e9ff20383a8ad06f91dccf721a453ba
Blad3Mak3r <blademaker.live@gmail.com>
2020-04-14 21:05:59 +0200

    Create Utils

---
+++ src/api/utils.ts
@@ -0,0 +1,31 @@
+import { Meme } from "./interfaces/meme";
+import Axios from "axios";
+
+export function randomNumber(number: number): number {
+    return Math.floor(Math.random() * number);
+}
+
+export function checkURL(url: string): boolean {
+    return(url.match(/\.(jpeg|jpg|gif|png)$/) != null);
+}
+
+export async function buildMeme(url: string) {
+    try {
+        const result = await Axios.get(url)
+
+        if (result.status === 200) {
+            const children = result.data.data.children;
+            let post = children[randomNumber(children.length)].data;
+
+            while(!checkURL(post.url)) {
+                post = children[randomNumber(children.length)].data;
+            }
+
+            return new Meme(post);
+        } else {
+            throw new Error("Cannot get memes Yaiks...")
+        }
+    } catch(e) {
+        throw e;
+    }
+}
