9c34599eb69082b055d2dc05d3dd7cd9fbd608b9
Blad3Mak3r <blademaker.live@gmail.com>
2020-04-14 21:06:24 +0200

    Create tests

---
+++ examples/test.ts
@@ -0,0 +1,22 @@
+import { getRandomMeme, getLocalRandomMeme } from '../src/index';
+
+async function init() {
+    try {
+        console.log("\n========================================");
+        console.log("=          Gettin Random Meme          =");
+        console.table(await getRandomMeme());
+        console.log("\n========================================");
+        console.log("=       Gettin Random Anime Meme       =");
+        console.table(await getRandomMeme("anime"));
+        console.log("\n========================================");
+        console.log("=     Gettin Random Spongebob Meme     =");
+        console.table(await getRandomMeme("spongebob"));
+        console.log("\n========================================");
+        console.log("=     Gettin Spanish Spongebob Meme     =");
+        console.table(await getLocalRandomMeme("es"));
+    } catch(e) {
+        throw e;
+    }
+}
+
+init().then(() => console.log("Done")).catch(() => console.log("Done"));
