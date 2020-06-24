9e39843c568fc024afab655c1ce4a7e84e7b5900
Blad3Mak3r <blademaker.live@gmail.com>
2020-06-24 10:28:12 +0200

    Support for more national reddits

--- src/api.ts
+++ src/api.ts
@@ -1,12 +1,15 @@
 import { randomNumber, buildMeme } from './utils';
 
 const subreddits = {
-  en: ['memes', 'dankmemes', 'meirl'],
-  es: ['memesesp', 'memesenespanol', 'spanishmeme'],
-  fr: ['FrenchMemes'],
-  ru: ['CommunismMemes', 'YouSeeComrade'],
-  de: ['GermanMemes'],
-  it: ['italianmemes'],
+  "br": ['MemesBrasil', 'eu_nvr', 'DiretoDoZapZap'],
+  "de": ['GermanMemes'],
+  "en": ['memes', 'dankmemes', 'meirl'],
+  "es": ['memesesp', 'memesenespanol', 'spanishmeme'],
+  "fr": ['FrenchMemes'],
+  "in": ['IndianDankMemes', 'IndianMeyMeys', 'desimemes'],
+  "it": ['italianmemes'],
+  "ru": ['CommunismMemes', 'YouSeeComrade'],
+  "tr": ['burdurland', 'TurkeyJerky', 'TurkishMemeCommunity'],
 };
 
 /**
@@ -29,16 +32,22 @@ function randomReddit(lang?: string | string[]): string {
   if (!lang) return subreddits.en[randomNumber(subreddits.en.length)];
 
   switch (lang.toString()) {
+    case 'br':
+      return subreddits.de[randomNumber(subreddits.br.length)];
+    case 'de':
+      return subreddits.de[randomNumber(subreddits.de.length)];
     case 'es':
       return subreddits.es[randomNumber(subreddits.es.length)];
     case 'fr':
       return subreddits.fr[randomNumber(subreddits.fr.length)];
-    case 'de':
-      return subreddits.de[randomNumber(subreddits.de.length)];
+    case 'in':
+      return subreddits.fr[randomNumber(subreddits.in.length)];
     case 'it':
       return subreddits.it[randomNumber(subreddits.it.length)];
     case 'ru':
       return subreddits.ru[randomNumber(subreddits.ru.length)];
+    case 'tr':
+      return subreddits.ru[randomNumber(subreddits.tr.length)];
     default:
       return subreddits.en[randomNumber(subreddits.en.length)];
   }
